ecd5775457f4e9a096fd4f606416efbf5716c1a2
sitaramnandi <sitarampravashnandi@gmail.com>
2024-03-19 00:00:52 +0530

    update

--- myprofile/settings.py
+++ myprofile/settings.py
@@ -149,7 +149,9 @@ STATIC_URL = 'static/'
 LOGOUT_REDIRECT_URL ="/"
 LOGIN_REDIRECT_URL ="/about"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp', 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
